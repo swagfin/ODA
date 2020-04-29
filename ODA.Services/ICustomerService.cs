17cd94dd2ebde7144c6a28e92cd5f48c605425e5
Wainaina George (Swagfin) <39550012+swagfin@users.noreply.github.com>
2020-04-29 18:59:22 +0300

    Added ODA Services

---
+++ ODA.Services/ICustomerService.cs
@@ -0,0 +1,23 @@
+﻿using ODA.Entity;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ODA.Services
+{
+    public interface ICustomerService
+    {
+        void Add(Customer customer);
+        Task AddAsync(Customer customer);
+        IEnumerable<Customer> GetAll();
+        Task<IEnumerable<Customer>> GetAllAsync();
+        Customer Get(int Id);
+        Task<Customer> GetAsync(int Id);
+        void Update(Customer customer);
+        Task UpdateAsync(Customer customer);
+        void Remove(int Id);
+        Task RemoveAsync(int Id);
+        void Remove(Customer customer);
+        Task RemoveAsync(Customer customer);
+    }
+}
---
+++ ODA.Services/IOrderService.cs
@@ -0,0 +1,23 @@
+﻿using ODA.Entity;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ODA.Services
+{
+    public interface IOrderService
+    {
+        void Add(Order order);
+        Task AddAsync(Order order);
+        IEnumerable<Order> GetAll();
+        Task<IEnumerable<Order>> GetAllAsync();
+        Order Get(int Id);
+        Task<Order> GetAsync(int Id);
+        void Update(Order order);
+        Task UpdateAsync(Order order);
+        void Remove(int Id);
+        Task RemoveAsync(int Id);
+        void Remove(Order order);
+        Task RemoveAsync(Order order);
+    }
+}
