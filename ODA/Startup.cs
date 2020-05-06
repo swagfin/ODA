c085c5d958aadb8ee2ece3ebf9fb9048ab47890c
Wainaina George (Swagfin) <39550012+swagfin@users.noreply.github.com>
2020-05-06 13:20:08 +0300

    Reverted all Files removing the ODA Server

--- ODA/Startup.cs
+++ ODA/Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using ODA.DataAccess;
+using ODA.Context;
 
 namespace ODA
 {
